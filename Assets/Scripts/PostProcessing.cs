07efa325bc16e1089ceab7a075ccf3424ea3669a
Hyvärinen Markus TTK15SP <markushyvarinen@kamk.fi>
2017-12-02 16:10:55 +0200

    Känni päälle

---
+++ Assets/Scripts/PostProcessing.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class PostProcessing : MonoBehaviour {
+
+	public List<Material> EffectMaterials = new List<Material>();
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		foreach(Material mat in EffectMaterials){
+			if(mat == null) continue;
+
+			Graphics.Blit(src, dest, mat);
+		}
+	}
+}
