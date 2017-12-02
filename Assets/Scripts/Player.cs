e7a9ad60cd76cd562c416cf1e775c6d4dc2e0dd1
Hyvärinen Markus TTK15SP <markushyvarinen@kamk.fi>
2017-12-02 17:45:12 +0200

    Kännit muuttujana, pelaaja huojuu vähä Alan Wake tason almppu

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 	public float maxWarmth;
 	public float warmthDepletePerSecond;
 	public float warmthGainPerSecond;
-    public AudioClip callSound;
+	public AudioClip callSound;
 
-    AudioSource audioSource;
+	AudioSource audioSource;
 
-    Roni roni;
+	Roni roni;
 
 	private float _currentWarmth;
 	public float currentWarmth {
@@ -19,42 +19,58 @@ public class Player : MonoBehaviour {
 			return _currentWarmth;
 		}
 		set {
-			_currentWarmth = Mathf.Clamp(value, 0, maxWarmth);
+			_currentWarmth = Mathf.Clamp (value, 0, maxWarmth);
 		}
 	}
 
 	public bool isDrinking = false;
 
+	[HideInInspector]
+	public float drunkness = 0f;
+	public float drunknessGainedPerSecond = 1f;
+	public float drunknessLostPerSecond = 0.1f;
+
+[Header("Drunkness effects")]
+	public float movementAngleRotation = 270f;
+	public float movementRandom = 5f;
+	
+
+	void Awake () {
+		GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().Init(this);
+	}
+
 	void Start () {
 		currentWarmth = maxWarmth;
-        audioSource = GetComponent<AudioSource>();
-        roni = FindObjectOfType<Roni>();
+		audioSource = GetComponent<AudioSource> ();
+		roni = FindObjectOfType<Roni> ();
 	}
 
 	void Update () {
-		if (Input.GetMouseButton(0)) {
+		if (Input.GetMouseButton (0)) {
 			isDrinking = true;
 			currentWarmth += warmthGainPerSecond * Time.deltaTime;
+			drunkness += (drunknessGainedPerSecond + drunknessLostPerSecond) * Time.deltaTime;
+			drunkness = Mathf.Clamp (drunkness, 0f, 1f);
 		}
 
-		if (!Input.GetMouseButton(0)) {
+		if (!Input.GetMouseButton (0)) {
 			isDrinking = false;
 		}
 
-        if (Input.GetMouseButtonDown(1)) {
-            StartCoroutine(CallRoni());
-        }
+		if (Input.GetMouseButtonDown (1)) {
+			StartCoroutine (CallRoni ());
+		}
 
 		if (currentWarmth > 0 && !isDrinking) {
 			currentWarmth -= warmthDepletePerSecond * Time.deltaTime;
+			currentWarmth -= drunknessLostPerSecond * Time.deltaTime;
 		}
 	}
 
-
-    IEnumerator CallRoni() {
-        audioSource.PlayOneShot(callSound);
-        yield return new WaitForSeconds(callSound.length);
-        roni.CalledRoni();
-    }
+	IEnumerator CallRoni () {
+		audioSource.PlayOneShot (callSound);
+		yield return new WaitForSeconds (callSound.length);
+		roni.CalledRoni ();
+	}
 
 }
--- Assets/Scripts/PostProcessing.cs
+++ Assets/Scripts/PostProcessing.cs
@@ -7,9 +7,11 @@ public class PostProcessing : MonoBehaviour {
 	public List<Material> EffectMaterials = new List<Material>();
 
 	private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		if(EffectMaterials.Count == 0) return;
+
 		foreach(Material mat in EffectMaterials){
 			if(mat == null) continue;
-
+			
 			Graphics.Blit(src, dest, mat);
 		}
 	}
