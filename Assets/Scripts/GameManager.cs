a0597234e9b6243a9f937d4e59e209db495df96d
KAMIT\ToniAal <turbohernemail@gmail.com>
2017-12-02 11:29:17 +0200

    Player & gamemanager scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    Player player;
+
+	void Start () {
+        player = FindObjectOfType<Player>();
+	}
+	
+	void Update () {
+		if (player.currentWarmth <= 0) {
+            ResetLevel();
+        }
+
+        if (Input.GetKeyDown("r")) {
+            ResetLevel();
+        }
+	}
+
+
+    public void ResetLevel() {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float maxWarmth;
+    public float warmthChangePerFrame;
+    public Text warmthIndicator;
+    public float currentWarmth;
+
+    bool isDrinking = false;
+
+
+	void Start () {
+        currentWarmth = maxWarmth;
+	}
+	
+	void Update () {
+        warmthIndicator.text = "Warmth: " + Mathf.RoundToInt(currentWarmth);
+        print(currentWarmth);
+        if (currentWarmth > 0 && !isDrinking) {
+            currentWarmth -= warmthChangePerFrame;
+        }
+
+        if (Input.GetMouseButton(0)) {
+            isDrinking = true;
+            currentWarmth += warmthChangePerFrame;
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            isDrinking = false;
+        }
+
+	}
+}
