2e4603496c2b1ee5b66ba3e0179daeec7e6c42cb
KAMIT\ToniAal <turbohernemail@gmail.com>
2017-12-03 11:05:04 +0200

    Title screen doned + added lights to tonitestet

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+    public Button playButton;
+    public Button exitButton;
+
+	void Start () {
+        Button pbtn = playButton.GetComponent<Button>();
+        Button ebtn = exitButton.GetComponent<Button>();
+        pbtn.onClick.AddListener(PlayGame);
+        ebtn.onClick.AddListener(ExitGame);
+	}
+
+    void PlayGame() {
+        SceneManager.LoadScene("ToniTestet");
+    }
+
+    void ExitGame() {
+        Application.Quit();
+    }
+}
